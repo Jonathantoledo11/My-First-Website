d47da5a471d02ffa63fc2c569e2207c806c0812c
Jonathan <jonathantoledo11@gmail.com>
2021-10-24 12:26:39 -0700

    Added headers and paragraphs in all html files, cleaned up css and javascript files

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h3 = document.createElement("h3");
-h3.textContent = "This Gif is a representation of how I felt getting throught this Prework.";
+h3.textContent = "This Gif is a representation of how I felt getting through this Prework.";
 
 document.querySelector("body").appendChild(h3);
